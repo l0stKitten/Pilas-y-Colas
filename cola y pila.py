34da9dbb279490249b096dedc020cca22d687f88
Daria Lopez <dlopezf@ulasalle.edu.pe>
2022-10-21 13:46:06 -0500

    PILAS Y COLAS

---
+++ cola y pila.py
@@ -0,0 +1,13 @@
+queue = []
+
+print(queue)
+queue.append('a')
+print(queue)
+queue.append('b')
+print(queue)
+queue.append('c')
+print(queue)
+
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
